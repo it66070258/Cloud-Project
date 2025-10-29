c9d243dbb4f19fd40d0b71c4cb0c503d28407e34
EXCompleteWorld <66070258@kmitl.ac.th>
2025-10-29 16:58:26 +0700

    cloud

---
+++ userdata.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
+GIT_REPO_URL="https://github.com/it66070258/Cloud-Project.git"
+
+# Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
+PROJECT_MAIN_DIR_NAME="Cloud-Project"
+
+# Clone repository
+git clone "$GIT_REPO_URL" "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
+
+cd "/home/ubuntu/$PROJECT_MAIN_DIR_NAME"
+
+# Make all .sh files executable
+chmod +x scripts/*.sh
+
+# Execute scripts for OS dependencies, Python dependencies, Gunicorn, Nginx, and starting the application
+./scripts/instance_os_dependencies.sh
+./scripts/python_dependencies.sh
+./scripts/gunicorn.sh
+./scripts/start_app.sh
