85ad360c133fc853bc872d5cf8f068e0766a7f41
PeerachM <66070301@kmitl.ac.th>
2025-10-31 00:09:30 +0700

    test

--- itshop/itshop/settings.py
+++ itshop/itshop/settings.py
@@ -116,6 +116,10 @@ CKEDITOR_CONFIGS = {
 # Internationalization
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LANGUAGE_CODE = 'en-us'
 
